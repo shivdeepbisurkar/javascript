9afc77f0de21e147dca3d44560cbf5e67621aa9a
shivdeepbisurkar <37465451+shivdeepbisurkar@users.noreply.github.com>
2023-12-20 05:34:40 +0000

    operations

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -1,11 +1,11 @@
 let score = "shivdeep"
 
-console.log(typeof score);
-console.log(typeof(score))
+// console.log(typeof score);
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN =Not a number
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = "shid"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=> true 0=>false
 //""=> false
@@ -22,5 +22,48 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringnumber= String(someNumber)
-console.log(stringnumber);
-console.log(typeof(stringnumber));
+// console.log(stringnumber);
+// console.log(typeof(stringnumber));
+
+
+// ***************************** Operations ************************
+
+let value =3
+let negValue = -value;
+// console.log(negValue);
+
+//consolg.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1="hello"
+let str2=" shivdeep"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2")
+console.log(1+2+"2");//32
+console.log("1"+2+2);//122
+
+console.log(+true)
+console.log(+"")
+
+// let num1,num2,num3
+
+// num1=num2=num3= 2+2
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
