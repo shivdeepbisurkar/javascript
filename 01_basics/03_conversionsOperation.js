926d8163818ce3d1b37793972296fed704d14aa2
shivdeepbisurkar <37465451+shivdeepbisurkar@users.noreply.github.com>
2023-12-20 05:14:49 +0000

    convertion of data type

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,26 @@
+let score = "shivdeep"
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN =Not a number
+//true => 1; false => 0;
+
+let isLoggedIn = "shid"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true 0=>false
+//""=> false
+// "hitesh"=>true
+
+let someNumber = 33
+let stringnumber= String(someNumber)
+console.log(stringnumber);
+console.log(typeof(stringnumber));
