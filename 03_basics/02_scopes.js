798274c14ecc77557a39b598c0e3699856ee6afe
Shivdeep Bisurkar <37465451+shivdeepbisurkar@users.noreply.github.com>
2023-12-25 23:07:07 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,13 @@ if(true){
  console.log(a);
 // console.log(b);
 console.log(c);
+
+console.log(addone(5))
+function addone(num){
+    return num+1;
+}
+
+//addTwo(5)                                 //we can't access function when we declare it as below because of concept hoisting. 
+const ans=function addTwo(num){
+    return num+1;
+}
