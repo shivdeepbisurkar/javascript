47688687815889b634b69a35542eddc5308d41be
shivdeepbisurkar <37465451+shivdeepbisurkar@users.noreply.github.com>
2023-12-25 19:10:08 +0000

    04_object

---
+++ 02_basics/04_objecct.js
@@ -0,0 +1,33 @@
+const tinderuser=new Object();
+
+tinderuser.id="123"
+tinderuser.name="Shivdeep"
+console.log(tinderuser);
+
+const newUser={
+    id:"123",
+    name:{
+        username:{
+            firtname:"Shivdeep",
+            lastname:"Bisurkar"
+        }
+    }
+}
+// console.log(newUser.name.username.firtname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+
+// const obj3={obj1,obj2}
+// console.log(obj3);
+
+// //const obj3 = Object.assign({},obj1,obj2)
+// const obj3={...obj1, ...obj2}
+// console.log(obj3);
+
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLogged'));
